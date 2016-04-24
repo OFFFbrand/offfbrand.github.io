be0fa7f047f331c94206c5c13f03dc0c3ffd6518
Kartik Patel <kartikpatel@Kartiks-MacBook-Pro-3.local>
2016-04-24 14:48:03 -0500

    Updating footer layout and body font to Muli

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,10 +1,14 @@
-//
-// Foundation-Jekyll - Javascripts
-// Author: @aaronkwhite
-//
-
 // Init Foundation
 $(document).foundation();
 
 // Init Highlight.js
 //hljs.initHighlightingOnLoad();
+
+$(document).ready(function() {
+  spacer_sizer();
+  $(window).resize(spacer_sizer);
+});
+
+function spacer_sizer() {
+  $(".spacer").height($(".footer").outerHeight());
+}
